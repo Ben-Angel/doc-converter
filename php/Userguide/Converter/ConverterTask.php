642ee04716e2a4a7755e812ca1724c1c34af3b41
Dieter Raber <dieter@taotesting.com>
2014-11-16 18:12:32 +0100

    Added method buildTree

--- php/Userguide/Converter/ConverterTask.php
+++ php/Userguide/Converter/ConverterTask.php
@@ -3,6 +3,7 @@
 namespace Userguide\Converter;
 
 use Jig\Utils\FsUtils;
+use Jig\Utils\StringUtils;
 use Symfony\Component\Yaml\Yaml;
 use Userguide\Converter\PluginFactory;
 
@@ -28,6 +29,15 @@ class ConverterTask
         include($configFile);
         $config = Yaml::parse(ob_get_clean());
 
+
+        $ymlTree = Yaml::parse(
+            file_get_contents($config['paths']['base'] . $config['paths']['trees'] . '/toc.yml')
+        );
+
+        $tree             = $this->buildNode('Table of Contents', 'class');
+        $tree['children'] = $this->buildTree($ymlTree);
+
+
         foreach($config['paths'] as &$path) {
             $path = FsUtils::normalizePath($path);
         }
@@ -50,10 +60,14 @@ class ConverterTask
      * @param $inputPath
      * @return array
      */
-    protected function getFileListing($inputPath) {
+    protected function getFileListing($inputPath)
+    {
 
-        $listing = array();
-        $rawListing = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($inputPath), \RecursiveIteratorIterator::SELF_FIRST);
+        $listing    = array();
+        $rawListing = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($inputPath),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
         foreach ($rawListing as $mdFile => $cursor) {
             if ('md' !== $cursor->getExtension()) {
                 continue;
@@ -61,6 +75,54 @@ class ConverterTask
             $listing[] = $mdFile;
         }
         return $listing;
+    }
+
+    /**
+     * @param $nodeName
+     * @param $nodeType
+     * @param string $parentHref
+     * @return array
+     */
+    protected function buildNode($nodeName, $nodeType, $parentHref='')
+    {
+        $normalized = StringUtils::removeSpecChars($nodeName);
+        $node       = array(
+            'data'       => $nodeName,
+            'type'       => $nodeType,
+            'attributes' => array(
+                'id'    => $normalized,
+                'class' => 'node-' . $nodeType,
+                'href'  => $parentHref . '/' . $normalized,
+            )
+        );
+        return $node;
+    }
+    
+
+    /**
+     * @param $ymlTree
+     * @param string $parentHref
+     * @return array
+     */
+    protected function buildTree($ymlTree, $parentHref = '')
+    {
+        $result = array();
+        foreach ($ymlTree as $key => $branch) {
+            //files
+            if (!is_array($branch)) {
+                $result[] = $this->buildNode($branch, 'instance', $parentHref);
+            } else {
+                //yaml adds an extra array level with numeric key, so remove this here
+                if (is_numeric($key)) {
+                    $key    = key($branch);
+                    $branch = current($branch);
+                }
+                $node             = $this->buildNode($key, 'class', $parentHref);
+                $node['children'] = $this->buildTree($branch, $node['attributes']['href']);
+                $result[]         = $node;
+            }
+        }
+        return $result;
     }
 
 }
