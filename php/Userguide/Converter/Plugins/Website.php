aac5331abe0ded14d953b78a09b3f769dcd6e572
Michael <Komarouski@1pt.com>
2014-12-05 11:17:34 +0300

    Website plugin now using url maps for links and images during generation html

--- php/Userguide/Converter/Plugins/Website.php
+++ php/Userguide/Converter/Plugins/Website.php
@@ -8,9 +8,11 @@
 
 namespace Userguide\Converter\Plugins;
 
+use Jig\Utils\Console;
 use Userguide\Converter\PluginAbstract;
 use Userguide\Converter\PluginInterface;
 use Jig\Utils\FsUtils;
+use Userguide\Helpers\Indexer;
 
 
 class Website extends PluginAbstract implements PluginInterface {
@@ -20,7 +22,10 @@ class Website extends PluginAbstract implements PluginInterface {
         foreach($fileListing as $mdFile) {
             $outputPath = $this->getOutputPath(dirname($mdFile));
             FsUtils::mkDir($outputPath);
-            system(sprintf('%s -f markdown -t html %s > %s', $this->options['bin'], $mdFile, $outputPath . '/' . basename($mdFile, '.md') . '.html'), $retVal);
+            system(sprintf('%s -f markdown -t html %s > %s',
+                $this->options['bin'],
+                $mdFile . ' ' . $this->paths['base'] . $this->paths['trees'] . DIRECTORY_SEPARATOR . Indexer::FILE_MAP_LINKS_NESTED,
+                $outputPath . '/' . basename($mdFile, '.md') . '.html'), $retVal);
         }
     }
 } 
